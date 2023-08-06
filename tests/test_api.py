f5468fb085aead9b75dc98600e60f9f88e324ca6
Pirat <vasya.tishhenko.2001@mail.ru>
2023-08-06 22:20:02 +0500

    Allure added

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,11 +1,13 @@
+import numbers
+import allure
 import requests
 import jsonschema
 
-site_url = "https://petstore.swagger.io/#/store"
 base_url = "https://petstore.swagger.io/v2"
 order_id = 1
 
 
+@allure.story("Create order")
 def test_post_order():
     new_order = {
         "id": order_id,
@@ -18,8 +20,11 @@ def test_post_order():
     response = requests.post(f'{base_url}/store/order', json=new_order)
     assert response.status_code == 200, "Not found element"
     assert response.json() == new_order
+    allure.attach(str(response.status_code), "Response Status Code", allure.attachment_type.TEXT)
+    allure.attach(response.text, "Response Content", allure.attachment_type.TEXT)
 
 
+@allure.story("Get order")
 def test_get_order():
     response = requests.get(f'{base_url}/store/order/{order_id}')
     assert response.status_code == 200, "Not found element"
@@ -37,36 +42,34 @@ def test_get_order():
         "required": ["id", "petId", "quantity", "shipDate", "status", "complete"]
     }
     jsonschema.validate(response.json(), order_schema)
+    allure.attach(str(response.status_code), "Response Status Code", allure.attachment_type.TEXT)
+    allure.attach(response.text, "Response Content", allure.attachment_type.TEXT)
 
-
+@allure.story("Delete order")
 def test_delete_order():
     response = requests.delete(f'{base_url}/store/order/{order_id}')
     assert response.status_code == 200, "Not found element"
+    allure.attach(str(response.status_code), "Response Status Code", allure.attachment_type.TEXT)
+    allure.attach(response.text, "Response Content", allure.attachment_type.TEXT)
 
 
+@allure.story("Deleted order check")
 def test_get_deleted_order():
     response = requests.get(f'{base_url}/store/order/{order_id}')
     assert response.status_code == 404, "Element was found"
+    allure.attach(str(response.status_code), "Response Status Code", allure.attachment_type.TEXT)
+    allure.attach(response.text, "Response Content", allure.attachment_type.TEXT)
 
 
+@allure.story("Inventory check")
 def test_check_inventory():
     response = requests.get(f'{base_url}/store/inventory')
     assert response.status_code == 200
-    print(response.json())
-    answer_schema = {
-        "type": "object",
-        "properties": {
-            'sold': {"type": "number"},
-            '1': {"type": "number"},
-            'Sold': {"type": "number"},
-            'random_pet_status': {"type": "number"},
-            'string': {"type": "number"},
-            'unavailable': {"type": "number"},
-            'pending': {"type": "number"},
-            'available': {"type": "number"},
-            'not available': {"type": "number"}
-        },
-        "required": ["sold", "1", "Sold", "random_pet_status", "string", "unavailable", "pending",
-                     "available", "not available"]
-    }
-    jsonschema.validate(response.json(), answer_schema)
+
+    excepted_field = "sold"
+    assert excepted_field in response.json(), "Не передано поле 'sold'"
+
+    if not isinstance(response.json()[excepted_field], numbers.Number):
+        raise ValueError("Value is not digit")
+    allure.attach(str(response.status_code), "Response Status Code", allure.attachment_type.TEXT)
+    allure.attach(response.text, "Response Content", allure.attachment_type.TEXT)
